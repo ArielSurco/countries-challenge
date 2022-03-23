1f788763d352826be432efb280160adf8892111d
ArielSurco <arielsurcoa@gmail.com>
2022-03-23 17:49:23 -0300

    Listing countries in Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,20 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { getCountryByName } from '../redux/actions/actions';
+import { getAllCountries } from '../redux/actions/actions';
 import CountriesCard from '../components/countries/CountriesCard';
 
 const Home = () => {
-  const country = useSelector((state) => state.country);
+  const countries = useSelector((state) => state.countries);
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(getCountryByName('Argentina'));
+    dispatch(getAllCountries('Argentina'));
   }, []);
   return (
-    (country?.name && <CountriesCard country={country} mode="detail" />) || (
-      <div />
-    )
+    <div className="countries-container">
+      {countries?.map((country) => (
+        <CountriesCard country={country} mode="preview" />
+      ))}
+    </div>
   );
 };
 
